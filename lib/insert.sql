a533a8c94053f6bcdaf6f16ad0cece637339f87a
Michelle <nyamburamichelle400@gmail.com>
2024-06-03 13:46:11 +0300

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+
+VALUES 
+ ("Mr. Chocolate", 20, "M", "dark brown", "calm", TRUE),
+ ("Rowdy", 18, "M", "dark brown", "happy", FALSE),
+ ("Tabitha", 16, "F", "dark brown", "sad", TRUE),
+ ("Sergeant Brown", 19, "M", "Green", "Slimy", TRUE),
+ ("Melissa", 15, "F", "dark brown", "goofy", TRUE),
+ ("Grinch", 2, "M", "Black", "Grinchy", TRUE),
+ ("Wendy", 6, "F", "Blue", "naive", TRUE),
+ (null, 20, "M", "black", "aggressive", FALSE);
